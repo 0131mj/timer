ef5022fb3873a40858b222d119ca8079eb8eb1ac
mjbae <0131mj@gmail.com>
2018-08-29 07:51:28 +0900

    - 시계형태로 시간변화 구현

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import ControlButtons from './Components/ControlButtons'
+
 class App extends Component {
+
 
   constructor(props){
     super(props);
-    state:{
-      // hours : '',
-      // minutes : '',
-      // seconds: '',
+    this.state = {
+      limitTime: 0,
+      hours : 0,
+      minutes : 0,
+      seconds: 0,
     }
   }
 
+  componentDidMount(){
+      setInterval(
+          ()=>{this.setState({
+              hours     : (new Date()).getHours(),
+              minutes   : (new Date()).getMinutes(),
+              seconds   : (new Date()).getSeconds()
+          })},
+      1000)
+  }
+
   render() {
-      let currentTime = new Date();
-      let hours = currentTime.getHours();
-      let minutes = currentTime.getMinutes();
-      let seconds = currentTime.getSeconds();
-
-      // setTimeout(function (){alert("hello");},3000);
-      setInterval(()=>{
-          seconds = currentTime.getSeconds();
-      },1000);
-
     return (
       <div className="App">
-
-          {hours} : {minutes} : {seconds}
+          <div>
+              설정시간 : <span>5</span> 분
+              <button>▲</button>
+              <button>▼</button>
+          </div>
+          {this.state.hours} : {this.state.minutes} : {this.state.seconds}
 
           <ControlButtons/>
+          <br/>
 
-          1초마다 카운트가 되도록 하기
+
+          <br/> 1초마다 카운트가 되도록 하기
           {/*카운트 시작*/}
           {/*카운트 종료 알림*/}
           {/*카운트 초기화*/}
