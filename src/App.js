e9385efd931594b8841a6292f79d2e6078bcb7b4
mjbae <0131mj@gmail.com>
2018-08-30 08:30:44 +0900

    - 사용자가 입력한 값 카운트다운 구현

--- src/App.js
+++ src/App.js
@@ -4,38 +4,85 @@ import ControlButtons from './Components/ControlButtons'
 
 class App extends Component {
 
-
-  constructor(props){
-    super(props);
-    this.state = {
-      limitTime: 0,
-      hours : 0,
-      minutes : 0,
-      seconds: 0,
+    constructor(props){
+        super(props);
+        this.state = {
+          // limitTime: 0,
+          // hours : 0,
+          // minutes : 0,
+          // seconds: 0,
+            inputTime: 20,
+            remainTime : 20,
+        }
     }
-  }
 
-  componentDidMount(){
-      setInterval(
-          ()=>{this.setState({
-              hours     : (new Date()).getHours(),
-              minutes   : (new Date()).getMinutes(),
-              seconds   : (new Date()).getSeconds()
-          })},
-      1000)
-  }
+    componentDidMount(){
 
+    }
+
+    /**
+     * TODO : clearInterval (0초 되면),
+     * 인풋값을 계산해서 초로 변환하기
+     * */
+
+    countDown(){
+        setInterval(
+            ()=>{this.setState({
+                ...this.state,
+                remainTime   : this.state.remainTime -1
+            })},
+        1000);
+    }
+
+    countStop(){
+        clearInterval(this.countDown);
+    }
+
+    increaseTime(){
+      this.setState({
+          ...this.state,
+          inputTime : this.state.inputTime + 5,
+          remainTime : this.state.remainTime + 5,
+      })
+    }
+
+    decreaseTime(){
+      if(this.state.inputTime > 0){
+          this.setState({
+              ...this.state,
+              inputTime : this.state.inputTime - 5,
+              remainTime : this.state.remainTime - 5,
+          })
+      }
+    }
+
+
+    /**
+     *
+     * 필요한 파라미터
+     *
+     * 사용자가
+     * 입력한
+     * 시간,
+     *
+     * 남은 시간(현재 시간),
+     * 변하게 하는 함수,
+     *
+     * */
   render() {
     return (
       <div className="App">
           <div>
-              설정시간 : <span>5</span> 분
-              <button>▲</button>
-              <button>▼</button>
+              설정시간 : <span>{this.state.inputTime}</span> 분
+              <button onClick={()=>{this.increaseTime()}}>▲</button>
+              <button onClick={()=>{this.decreaseTime()}}>▼</button>
           </div>
-          {this.state.hours} : {this.state.minutes} : {this.state.seconds}
-
-          <ControlButtons/>
+          {this.state.remainTime} : {'00'}
+          <div>
+              <button onClick={()=>this.countDown()}>시작</button>
+              <button onClick={()=>this.countStop()}>중지</button>
+          </div>
+          {/*<ControlButtons/>*/}
           <br/>
 
 
